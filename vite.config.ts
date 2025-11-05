60a0b2ec77b7e451e42e01b57014ba3fcfe35b91
MathieuAumont <mathieu.aumont@live.ca>
2025-11-05 14:34:09 -0500

    jf

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   base: process.env.NODE_ENV === 'production' ? '/CamilleChancerelMassotherapie/' : '/',
   build: {
-    outDir: 'dist',
+    outDir: 'docs',
     assetsDir: 'assets',
   }
 })
