3237e58638b2cc235ce8f37202f8ac0d8baf014d
MathieuAumont <mathieu.aumont@live.ca>
2025-11-05 14:24:42 -0500

    new dist

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/CamilleChancerelMassotherapie/'
+  base: process.env.NODE_ENV === 'production' ? '/CamilleChancerelMassotherapie/' : '/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  }
 })
